53533e8b86b2a25e88abc44c9eb6deff002fa82b
aihongwei <ufo008ahw@163.com>
2014-08-29 12:06:18 +0800

    clear garbage

--- algorithm/dataStructure.h
+++
@@ -1,4 +0,0 @@
-// Author: ufo008ahw
-// Last modified: 2013-09-15 22:15
-
-struct dataStructure;
--- algorithm/graph.h
+++
@@ -1,8 +0,0 @@
-// Author: ufo008ahw
-// Last modified: 2013-09-15 22:15
-
-extern int MST(int n, int tree[]);
-extern void dijkstra(int n, int path[]);
-extern void floyd(int n);
-extern int maxmatch();
-
